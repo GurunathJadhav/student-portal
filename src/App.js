0797e18d7c8976bc9a79e323defdb02e36862d37
Gurunath Jadhav <gurunath.jadhav.842@gmail.com>
2025-05-05 00:23:17 +0530

    modify app.css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
+        <div className='main'>
+          <div className='content'>
+            <h2>WELCOME TO JENKINS REACT APP</h2>
+          </div>
+        </div>
 
       </div>
     </div>
